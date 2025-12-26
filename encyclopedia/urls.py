a3c20e5c80813d7a00e6d87889c566a6b7e8bbb8
KF <kauamorenofilipe123@gmail.com>
2025-12-26 10:06:35 -0300

    001(contaim severals bugs)

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from encyclopedia import util
+from . import views
+from django.urls import path
 from . import views
 
 app_name = "encyclopedia"
@@ -6,6 +9,8 @@ app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("search/", views.search, name="search"),
+    path("newentrymaker/", views.newentrymaker, name="newentrymaker"),
+    path("edit/<str:entry>/", views.edit, name="edit"),
     path("wiki/<str:title>/", views.entry, name="entry"),
     path("random/", views.random, name="random"),
 
