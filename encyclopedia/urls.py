d6ed0bcdfaca6208c0350acf0f62e0b61f707af4
KF <kauamorenofilipe123@gmail.com>
2025-12-29 18:02:13 -0300

    007

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,11 +4,11 @@ from . import views
 app_name = "encyclopedia"
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("search/", views.search, name="search"),
-    path("newentrymaker/", views.newentrymaker, name="newentrymaker"),
-    path("edit/<str:entry>/", views.edit, name="edit"),
-    path("wiki/<str:title>/", views.entry, name="entry"),
-    path("random/", views.random, name="random"),
+    path("", views.index, name="index"), # index url path #
+    path("search/", views.search, name="search"), # search function page url path #
+    path("newentrymaker/", views.newentrymaker, name="newentrymaker"), # entry maker page url path #
+    path("edit/<str:entry>/", views.edit, name="edit"), # edit function path #
+    path("wiki/<str:title>/", views.entry, name="entry"), # entry visualization url path #
+    path("random/", views.random, name="random"), # random page function #
 
 ]
