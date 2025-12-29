bd1e7ef36820c0bafeb4a4f844c1dee74abf1ac0
KF <kauamorenofilipe123@gmail.com>
2025-12-29 18:20:25 -0300

    009

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# site urls #
 urlpatterns = [
     path("", include("encyclopedia.urls")),
     path('admin/', admin.site.urls),
